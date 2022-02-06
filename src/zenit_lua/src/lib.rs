ebd2b895f75b7ad7f0e4f47c1afb3a2eb4ad2d49
Natalia Cholewa <natanalt@protonmail.com>
2022-02-06 01:25:36 +0100

    Add automated unit tests through GitHub Actions  * also mute some really annoying warnings from bindgen code  * and remove the .vscode directory which was supposed to do it    for rust-analyzer, but failed

--- src/zenit_lua/src/lib.rs
+++ src/zenit_lua/src/lib.rs
@@ -19,6 +19,7 @@ pub mod ffi {
     #![allow(non_upper_case_globals)]
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
+    #![allow(deref_nullptr)]
     include!(concat!(env!("OUT_DIR"), "/lua_bindings.rs"));
 }
 
