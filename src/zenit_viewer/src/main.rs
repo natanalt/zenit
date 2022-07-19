5634bca4a514ecc78f3a417e78c0b63c64d33377
Natalia Cholewa <natanalt@protonmail.com>
2022-07-19 19:27:45 +0200

    Transition the Zenit Viewer app into GTK4

--- src/zenit_viewer/src/main.rs
+++ src/zenit_viewer/src/main.rs
@@ -1,66 +1,30 @@
-use egui::*;
-use viewers::Viewer;
-
-mod viewers;
-
-struct ZenitViewer {
-    status_bar: String,
-    viewers: Vec<Box<dyn Viewer>>,
-    current_viewer: usize,
-}
-
-impl ZenitViewer {}
-
-impl Default for ZenitViewer {
-    fn default() -> Self {
-        Self {
-            status_bar: "💖 Welcome to the data browser!".to_string(),
-            viewers: viewers::default_viewers(),
-            current_viewer: 0,
-        }
-    }
-}
-
-impl eframe::App for ZenitViewer {
-    fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
-        TopBottomPanel::top("TabBrowser")
-            .resizable(false)
-            .show(ctx, |ui| {
-                ui.horizontal(|ui| {
-                    egui::widgets::global_dark_light_mode_switch(ui);
-                    ui.separator();
-
-                    ui.label("✨ Zenit Viewer");
-                    ui.separator();
-
-                    for (i, viewer) in self.viewers.iter().enumerate() {
-                        let response =
-                            ui.selectable_label(i == self.current_viewer, viewer.get_name());
-                        if response.clicked() {
-                            self.current_viewer = i;
-                        }
-                    }
-                });
-            });
-        
-        TopBottomPanel::bottom("StatusBar").show(ctx, |ui| {
-            ui.horizontal(|ui| ui.label(&self.status_bar));
-        });
-
-        self.viewers[self.current_viewer].update(ctx, frame, &mut self.status_bar);
-    }
-}
+use gtk4::{prelude::*, Application, ApplicationWindow, Button};
 
 fn main() {
-    eframe::run_native(
-        "Zenit Data Viewer",
-        eframe::NativeOptions {
-            initial_window_size: Some(vec2(800.0, 600.0)),
-            resizable: true,
-            ..Default::default()
-        },
-        Box::new(|cc| {
-            Box::new(ZenitViewer::default())
-        }),
-    );
+    let app = Application::builder()
+        .application_id("zenit_engine.viewer")
+        .build();
+    
+    app.connect_activate(|app| {
+        let button = Button::builder()
+            .label("nya")
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
+        
+        button.connect_clicked(move |_| {
+            println!("nya");
+        });
+
+        ApplicationWindow::builder()
+            .application(app)
+            .title("Zenit Data Viewer")
+            .child(&button)
+            .build()
+            .present();
+    });
+
+    app.run();
 }
